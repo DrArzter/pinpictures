24032456c8b12d3f72dd8ef7e2a178c8fa6cc8e2
HeenJoy <rom7martlp@gmail.com>
2024-02-21 17:25:26 +0300

    API posts 10%

--- api/posts/posts.php
+++ api/posts/posts.php
@@ -14,3 +14,29 @@ $dbname = $_ENV['dbname'];
 function sanitizeInput($input) {
     return $input !== null ? htmlspecialchars($input, ENT_QUOTES, 'UTF-8') : '';
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $post = json_decode(file_get_contents('php://input'), true);
+    $title = sanitizeInput($post['title']);
+    $picture = sanitizeInput($post['picture']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $page = sanitizeInput($_GET['page'])*20;
+    $posts = getPosts($page);
+    echo json_encode($posts);
+}
+
+function getPosts($page) {
+    global $secretKey, $dbip, $dbuser, $dbpassword, $dbname;
+    $conn = new mysqli($dbip, $dbuser, $dbpassword, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM posts ORDER BY created_at DESC LIMIT 20 OFFSET ?;");
+    $stmt->bind_param("n", $page);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
